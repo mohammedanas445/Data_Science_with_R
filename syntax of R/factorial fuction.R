e28a81a9902ddc521c7ac1b2bad1c7574886b1fc
Mohammed Anas <mohdanas22@iitk.ac.in>
2023-05-29 14:55:07 +0530

    14:54 29/5/2023

--- syntax of R/factorial fuction.R
+++ syntax of R/factorial fuction.R
@@ -16,12 +16,14 @@ FACtorial <- function(N){
 FACtorial(6)
 
 
-
+####################
 RHL_exp <- function(n){
   return((1+1/n)^n)
 }
 
 
+
+#########################
 data <- read.csv("SOC171A_Survey_Responses.csv")
 head(data) # to see the top of the data
 str(data) # structure of the data
@@ -33,3 +35,36 @@ indices=which((data$Category=="OBC-NCL")&(data$Marriage.preference=="Love Marria
 OBC_Mariage_data=data[indices,]
 OBC_Mariage_data
 
+
+
+#############################
+primes <- 2
+count <- 1
+m <- 3
+while( count <= 1000){
+  m.is.prime <- T
+  for(p in primes){
+    if(m%%p==0){
+      m.is.prime <- F
+      break
+    }
+  }
+  if(m.is.prime){
+    primes <- c(primes,m)
+    count <- count+1
+  }
+  m = m+2
+}
+primes
+
+
+##########################
+fibonacci.numbers=c(1,1)
+
+for(count in 3:500){
+  fibonacci.numbers[count]=fibonacci.numbers[count-1]+fibonacci.numbers[count-2]
+}
+fibonacci.numbers
+
+
+#####################################
